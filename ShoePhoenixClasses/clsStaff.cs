7c42429de367b5c045766efec628199d03a87aa0
P2423154 <P2423154@GWHXX280WIN018W>
2021-03-06 17:08:10 +0000

    Staff Management web form

--- ShoePhoenixClasses/clsStaff.cs
+++ ShoePhoenixClasses/clsStaff.cs
@@ -2,5 +2,12 @@
 {
     public class clsStaff
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int ContactNo { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string PostCode { get; set; }
     }
 }
